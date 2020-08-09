8f837ce035c2e5ba44eb11ef356f9be16985a70c
forexbotdev <forexbotdev@gmail.com>
2020-08-09 23:44:43 +0530

    cors config change

--- src/main/java/com/forexbot/api/config/WebSecurityConfig.java
+++ src/main/java/com/forexbot/api/config/WebSecurityConfig.java
@@ -2,6 +2,9 @@ package com.forexbot.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,7 +15,15 @@ import java.util.Arrays;
 
 @Component
 @Configuration
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // TODO : Update the security config once app is live
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.headers().frameOptions().sameOrigin();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
